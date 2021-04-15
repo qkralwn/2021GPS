44ed39069e3fd720b63836604f9c8047b7590f87
qkralwn721@naver.com <ppomi0661@naver.com>
2021-04-15 21:47:23 +0900

    개인프로젝트

---
+++ Match Game/Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour {
+    private Text text;
+    public int score = 0;
+
+    void Start() {
+        text = GetComponent<Text>();
+    }
+
+    void Update() {
+        text.text = ("SCORE\n" + score);
+        //Debug.Log(score);
+    }
+
+}
